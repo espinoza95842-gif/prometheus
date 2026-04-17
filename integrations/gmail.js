a4f81e2f238e31079c91e362f7316a03bafbab7b
espinoza95842-gif <espinoza95842@gmail.com>
2026-04-17 04:58:21 -0700

    Update gmail.js

--- integrations/gmail.js
+++ integrations/gmail.js
@@ -1,1 +1,14 @@
+import { google } from "googleapis";
 
+export async function fetchEmails(authClient) {
+
+  const gmail = google.gmail({ version: "v1", auth: authClient });
+
+  const res = await gmail.users.messages.list({
+    userId: "me",
+    maxResults: 10
+  });
+
+  return res.data.messages || [];
+
+}
